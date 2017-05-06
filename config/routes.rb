6a1d0711b4dee91b41e8211ad1bda7cfaed5d804
osiutino <osiutino@gmail.com>
2017-05-06 15:56:04 +0800

    quick routes declaration

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,7 @@
-# devise_for :admin_users, ActiveAdmin::Devise.config
-
-# namespace :site1 do
-#   devise_for :admin_users, ActiveAdmin::Devise.config
-# end
-#
-# namespace :site2 do
-#   devise_for :admin_users, ActiveAdmin::Devise.config
-# end
-#
-# namespace :site3 do
-#   namespace :demo do
-#     devise_for :admin_users, ActiveAdmin::Devise.config
-#   end
-# end
-
-# [[:site1], [:site2], [:site3, :demo]].map { |namespace| namespace.reverse.inject(active_admin_devise_proc) { |n, c| Proc.new{ namespace c, &n } } }
-
-def multi_active_admin_devise(*namespaces)
+def multi_namespaces(*namespaces, &block)
   namespaces.each do |namespace|
-    namespace.reverse.inject(-> { devise_for :admin_users, ActiveAdmin::Devise.config }) { |n, c| Proc.new { namespace c, &n } }.call
+    (namespace.is_a?(Array) ? namespace.reverse : [namespace])
+        .inject(Proc.new { block.call(namespace) if block && block.is_a?(Proc) }) { |n, c| Proc.new { namespace c, &n } }.call
   end
 end
 
@@ -26,7 +9,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   devise_for :admin_users, ActiveAdmin::Devise.config
-  multi_active_admin_devise([:site1], [:site2], [:site3, :demo])
+  multi_namespaces(:site1, :site2, [:site3, :demo]) do |namespace|
+    devise_for :admin_users, ActiveAdmin::Devise.config
+  end
 
   ActiveAdmin.routes(self)
 end
